ba3eb24046016539c35bcbb195456de666099ee1
puwow <qiaoguosong@sina.com>
2018-09-04 23:30:48 +0800

    添加Notebook控件

--- app.py
+++ app.py
@@ -2,9 +2,23 @@
 
 import wx
 
+class BookPage( wx.Panel ):
+    def __init__( self, parent, id=wx.ID_ANY ):
+        wx.Panel.__init__( self, parent=parent, id=id )
+
 class DemoFrame( wx.Frame ):
     def __init__( self, parent=None, id=wx.ID_ANY, title=u'wxPython Study', size=(800,600) , style=wx.DEFAULT_FRAME_STYLE ):
         wx.Frame.__init__( self, parent=parent, id=id, title=title, size=size, style=style )
+        panel = wx.Panel( self, id=wx.ID_ANY )
+        box = wx.BoxSizer( wx.VERTICAL )
+        book = wx.Notebook( panel, id=wx.ID_ANY )
+        page1 = BookPage(book)
+        page2 = BookPage(book)
+        book.AddPage( page1, u'页面1' )
+        book.AddPage( page2, u'页面2' )
+        box.Add( book, 1, wx.EXPAND|wx.ALL, 3 )
+
+        panel.SetSizer( box )
 
 if __name__ == '__main__':
     app = wx.App()
